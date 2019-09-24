b74de3c7e49c227bd00509114b43fa2e1b23cc57
MR-Stan <michaelrstanley87@gmail.com>
2019-09-23 21:39:24 -0500

    making progress... slowly...

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,4 @@
 // artist is selected
-// number of characters are determined
 // blanks are displayed as " " instead of "_"
 // initial song music is played
 // on win -> song changes to artist, picture updates, song title and artist are displayed, game resets
@@ -7,17 +6,37 @@
 
 // ********************************************************************** DOCUMENT OBJECTS *************************************************************** //
 
-    const guessNumText = document.getElementById("guessnum");   // number of guesses remaining
-
-    const winText = document.getElementById("wins");            // wins
+    const guessNumText = document.getElementById("guessnum");   // number of guesses remaining    
 
     const guessText = document.getElementById("guesses");       // letters already guessed
 
-    const wordText = document.getElementById("word");           // current unknown word w/ blanks and letters guessed
+    const bandText = document.getElementById("band");           // current unknown word w/ blanks and letters guessed
 
 // ------------------------------------------------------------------------------------------------------------------------------------------------------ //
 
-// ********************************************************************** FIXED ARRAYS ****************************************************************** //
+// ********************************************************************** GLOBAL VARIABLES ************************************************************** //
+
+    var computerGuess = [];              // computer's artist selection
+
+    var cgN = "";                        // computerGuess.name    
+
+    var guesses = [];                    // array of letters selected during current round
+
+    var userGuess = "";                  // user's letter guess
+
+    var guessesRem = 0;                 // guesses remaining this round 
+
+    var charNum = [];                    // array for number of characters of computerGuess
+
+    var band;                       // updated 'x' with spaces instead of commas
+
+    var ind;                        // index of space in cgN (computerGuess.name)
+
+    var letind;                     // index of letter in word that matches userGuess
+
+// ------------------------------------------------------------------------------------------------------------------------------------------------------ //
+
+// ********************************************************************** FIXED - are they really? ARRAYS ****************************************************************** //
 
     // array of the alphabet used to validate userGuess
     const alphaarr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -41,47 +60,45 @@
 
 // ------------------------------------------------------------------------------------------------------------------------------------------------------ //
 
-// ********************************************************************** GLOBAL VARIABLES ************************************************************** //
-
-    var computerGuess;              // computer's artist selection
-
-    var cgN;                        // reducing keystrokes    
-
-    var guesses;                    // array of letters selected during current round
-
-    var userWin;                    // total number of user wins
-
-    var userGuess;                  // user's letter guess
-
-    var guessesRem;                 // guesses remaining this round 
-
-    var charNum;                    // array for number of characters of computerGuess
-
-    var x;                          // updated charNum to string
-
-    var word;                       // updated 'x' with spaces instead of commas
-
-    var ind;                        // index of space in cgN (computerGuess.name)
-
-    var letind;                     // index of letter in word that matches userGuess
-
-// ------------------------------------------------------------------------------------------------------------------------------------------------------ //
-
 // ********************************************************************** FUNCTIONS ********************************************************************* //
 
-    // prints objects
+    // prints to document
     var print = function() {
         guessNumText.innerHTML = guessesRem;
         guessText.innerHTML = guesses;
-        wordText.innerText = word;
-        winText.innerText = userWin;
+        bandText.innerText = band;
+        
     }
 
     // convert to string and print
     var wordUp = function() {
-        x = charNum.toString();
-        word = x.replace(/,/g, " ");
+        let x = charNum.toString();
+        band = x.replace(/,/g, " ");
         print();
+    }
+
+    // initializes game
+    var initialize = function() {
+        computerGuess = artists[Math.floor(Math.random() * artists.length)];
+        cgN = computerGuess.name.toLowerCase();
+        guessesRem = parseInt(cgN.length - (cgN.length*0.2)); // determining # of guesses - possible to set difficulty
+        charNum = [];
+        guesses = [];
+        print();
+        // determining the number of characters in computerGuess
+        for (var i = 0; i < cgN.length; i++) {
+            charNum.push("_");
+        }
+        // checks for space in the artists name and converts the space to | - ***need to figure out how to keep the space instead of |
+        if (cgN.indexOf(" ") !== -1) {
+            ind = cgN.indexOf(" "); 
+            charNum.splice(ind, 1, " | ");
+            wordUp();
+
+        }
+        else {
+            wordUp();
+        }
     }
 
     // resets game
@@ -89,48 +106,51 @@
         computerGuess = artists[Math.floor(Math.random() * artists.length)];
         guessesRem = cgN.length;
         guesses = [];
-        print();
+        // determining the number of characters in computerGuess
+        for (var i = 0; i < cgN.length; i++) {
+            charNum.push("_");
+        }
+        // checks for space in the artists name and converts the space to | - ***need to figure out how to keep the space instead of |
+        if (cgN.indexOf(" ") !== -1) {
+            ind = cgN.indexOf(" "); 
+            charNum.splice(ind, 1, " | ");
+            wordUp();
+
+        }
+        else {
+            wordUp();
+        }
     }
 
+    var win = function() {
+        // put pic in html
+        // play song
+        // display artist name
+        // display song name
+        // winCount();
 
+    }
 
-// ------------------------------------------------------------------------------------------------------------------------------------------------------ //
-
-// ********************************************************************** INITIALIZE ******************************************************************** //
-
-    computerGuess = artists[Math.floor(Math.random() * artists.length)];
-    cgN = computerGuess.name;
-    guessesRem = cgN.length
-    charNum = []
-    guesses = []
-    userWin = 0
-    print();
+    // increases win counter by 1
+    var winCount = (function() {
+        var winCounter = 0;
+        const winText = document.getElementById("wins");  
+        winText.innerText = winCounter;
+        return function() {winCounter += 1; return winCounter}
+    })();
 
 // ------------------------------------------------------------------------------------------------------------------------------------------------------ //
 
 
 // ********************************************************************** GAME LOOP ********************************************************************* //
 
-if (true === true) {
-    console.log(computerGuess);
-    console.log(charNum);
 
-}
 
-// determining the number of characters in computerGuess
-for (var i = 0; i < cgN.length; i++) {
-    charNum.push("_");
-}
+initialize()
 
-// checks for space in the artists name and converts the space to | - ***need to figure out how to keep the space instead of |
-if (cgN.indexOf(" ") !== -1) {
-    ind = cgN.indexOf(" "); 
-    charNum.splice(ind, 1, " | ");
-    wordUp();
-}
-else {
-    wordUp();
-}
+
+
+
 
 // when a key is pressed
 document.onkeyup = function (event) {
@@ -142,7 +162,6 @@ document.onkeyup = function (event) {
         // verifying input is not a duplicate
         if (! guesses.includes(userGuess)) {
         guesses.push(userGuess); // keeping track of what letters have been used
-        print();
 
             // final guess doesn't match
             if (guessesRem === 1 && ! cgN.includes(userGuess)) {
@@ -152,7 +171,13 @@ document.onkeyup = function (event) {
 
             // letter matches a letter(s) in artist name
             else if (cgN.includes(userGuess)) {
-                console.log("got em coach");
+                // what cgn index is userguess found
+                for (var j = 0; j < (cgN.length); j++) {
+                    if (cgN[j] === userGuess) {
+                        charNum[j] = userGuess;
+                        wordUp();                      
+                    }
+                }
                 }
 
             // validation criteria met but guess doesn't match
@@ -160,19 +185,21 @@ document.onkeyup = function (event) {
                 guessesRem--;
                 print();
             }
+
         } 
 
         // duplicate entry
         else {
             alert("You've already used '" + userGuess + "'. Try again.");
+            print();
         }
     }
 
     // entry is not a letter
     else {
-        alert("Invalid entry. Enter a letter.");
+        alert("'" + event.key + "' is not a letter. Press one of these: " + alphaarr);
     }
 }
 
 
-
+// user computerGuess.name when win - this keeps capitalization
